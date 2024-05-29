f7ddee7f8b9c58d3f944ab3efb538895855bd906
Ikeoluwa Atulomah <adesinaikeoluwa4@gmail.com>
2024-05-28 22:25:03 -0700

    Sprint 10 module 1 done

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,24 +1,53 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import { name } from 'file-loader';
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
+const initialFormState = {
+  authorName: "",
+  quoteText: ""
+};
 
 // 👇 create your reducer function here
-
-export default function TodoForm({ createQuote = () => { } }) {
-  // 👇 use the reducer hook to spin up state and dispatch
-
-  const onChange = () => {
-    // 👇 implement
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload
+      return {
+        ...state,
+        [name]: value
+      };
+    }
+    case RESET_FORM:
+      return initialFormState;
+    default:
+      return state;
   }
+};
+
+export default function TodoForm({ createQuote }) {
+  // 👇 use the reducer hook to spin up state and dispatch
+  const [state, dispatch] = useReducer(formReducer, initialFormState);
+
+  const onChange = ({ target: { name, value } }) => {
+    // 👇 implement
+    dispatch({
+      type: CHANGE_INPUT,
+      payload: { name, value }
+    });
+  };
   const resetForm = () => {
     // 👇 implement
+    dispatch({ type: RESET_FORM });
   }
-  const onNewQuote = () => {
+  const onNewQuote = (e) => {
     // 👇 implement
+    e.preventDefault();
+    const { authorName, quoteText } = state
+    createQuote({ authorName, quoteText })
     resetForm()
   }
 
@@ -29,6 +58,7 @@ export default function TodoForm({ createQuote = () => { } }) {
       <label><span>Author:</span>
         <input
           type='text'
+          value={state.authorName}
           name='authorName'
           placeholder='type author name'
           onChange={onChange}
